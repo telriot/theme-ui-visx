6023f011f2fad6fff1962540daca98531a1ee88a
telriot <beniamino.tartarini@gmail.com>
2022-02-28 11:57:29 +0900

    Update cypress

--- cypress/integration/navigation.spec.ts
+++ cypress/integration/navigation.spec.ts
@@ -7,13 +7,4 @@ describe('Rendering and navigation', () => {
     cy.findAllByText(/modal drilldown/i).click();
     cy.findAllByText(/flight status/i).should('exist');
   });
-  it('should toggle between themes', () => {
-    cy.visit('http://localhost:3000');
-    cy.findAllByText(/dark/i).should('exist');
-    cy.findAllByText(/dark/i).click();
-    cy.findAllByText(/dark/i).should('not-exist');
-    cy.findAllByText(/light/i).should('exist');
-    cy.findAllByText(/light/i).click();
-    cy.findAllByText(/light/i).should('not-exist');
-  });
 });
