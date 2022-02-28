2f47ebe8c9439ff8404b290ea5fb9f338a6c1230
telriot <beniamino.tartarini@gmail.com>
2022-02-28 11:52:05 +0900

    Update cypress

--- README.md
+++ README.md
@@ -1,16 +1,4 @@
-# Next.js Front End starter pack
-
-Based on [`next-theme-ui-example`](https://github.com/system-ui/theme-ui/tree/master/examples/next).
-
-Featuring:
-
-- TypeScript
-- Next.js
-- ThemeUI
-- NProgress
-- Jest
-- Cypress
-- react-testing-library
+# Visx + ThemeUI Sandbox
 
 ## Testing
 
--- cypress/integration/navigation.spec.ts
+++ cypress/integration/navigation.spec.ts
@@ -2,9 +2,18 @@ describe('Rendering and navigation', () => {
   it('should navigate between pages without crashing', () => {
     cy.visit('http://localhost:3000');
     cy.findAllByText(/hello/i).should('exist');
-    cy.findAllByText(/style guide/i).click();
-    cy.findAllByText(/color palette/i).should('exist');
-    cy.findAllByText(/home/i).click();
-    cy.findAllByText(/hello/i).should('exist');
+    cy.findAllByText(/charts/i).click();
+    cy.findAllByText(/chart testing/i).should('exist');
+    cy.findAllByText(/modal drilldown/i).click();
+    cy.findAllByText(/flight status/i).should('exist');
+  });
+  it('should toggle between themes', () => {
+    cy.visit('http://localhost:3000');
+    cy.findAllByText(/dark/i).should('exist');
+    cy.findAllByText(/dark/i).click();
+    cy.findAllByText(/dark/i).should('not-exist');
+    cy.findAllByText(/light/i).should('exist');
+    cy.findAllByText(/light/i).click();
+    cy.findAllByText(/light/i).should('not-exist');
   });
 });
